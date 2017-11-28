b47323d6be551de630ba814d02df2c90ecf93ff9
Ryota Yamagishi <ddzsryotag@gmail.com>
2017-11-28 20:16:42 +0900

    apigateway通信成功

--- app/src/main/kotlin/jp/mochili/mochili/view/TopActivity.kt
+++ app/src/main/kotlin/jp/mochili/mochili/view/TopActivity.kt
@@ -2,12 +2,34 @@ package jp.mochili.mochili.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import jp.mochili.mochili.R
+import com.amazonaws.mobileconnectors.apigateway.ApiClientFactory
+import jp.mochili.mochili.model.AWS.AWSClient
+import jp.mochili.mochili.model.apigateway.MochiliClient
+import kotlin.concurrent.thread
+
 
 class TopActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top)
+
+        // test
+        thread {
+            try {
+                val credentialsProvider = AWSClient.getCredentialsProvider()
+                val client = ApiClientFactory()
+                        .credentialsProvider(credentialsProvider)
+                        .build<MochiliClient>(MochiliClient::class.java)
+                val mochilis = client.mymochilisGet("Karl")
+                val mochili = mochilis[0]
+                Log.d("test", mochili.mochiliId)
+            } catch(e: Exception) {
+                e.stackTrace
+            }
+        }
+
     }
 }

515a6a62cd95b68933295df74742693b67de09a0
Ryota Yamagishi <ddzsryotag@gmail.com>
2018-01-01 16:00:12 +0900

    設定画面のactivityを追加しました

---
+++ app/src/main/kotlin/jp/mochili/mochili/view/SettingActivity.kt
@@ -0,0 +1,13 @@
+package jp.mochili.mochili.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import jp.mochili.mochili.R
+
+class SettingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
---
+++ app/src/main/kotlin/jp/mochili/mochili/viewmodel/SettingViewModel.kt
@@ -0,0 +1,7 @@
+package jp.mochili.mochili.viewmodel
+
+/**
+ * Created by ryotayamagishi on 2018/01/01.
+ */
+class SettingViewModel {
+}

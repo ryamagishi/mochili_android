a9ad2d1390ef698e5015e5f01ff7d90f72802b95
Ryota Yamagishi <ddzsryotag@gmail.com>
2018-03-25 17:38:40 +0900

    友だち追加画面の作成途中

--- app/src/main/kotlin/jp/mochili/mochili/view/AddFriendActivity.kt
+++ app/src/main/kotlin/jp/mochili/mochili/view/AddFriendActivity.kt
@@ -1,13 +1,30 @@
 package jp.mochili.mochili.view
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import jp.mochili.mochili.R
+import jp.mochili.mochili.databinding.ActivityAddFriendBinding
+import jp.mochili.mochili.viewmodel.AddFriendViewModel
+import kotlinx.android.synthetic.main.activity_add_friend.*
 
 class AddFriendActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: AddFriendViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_friend)
+        val binding = DataBindingUtil.setContentView<ActivityAddFriendBinding>(this,
+                R.layout.activity_add_friend)
+        viewModel = AddFriendViewModel()
+        binding.viewmodel = viewModel
+
+        setView()
+    }
+
+    private fun setView() {
+        setSupportActionBar(my_toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "設定"
     }
 }
---
+++ app/src/main/kotlin/jp/mochili/mochili/viewmodel/AddFriendViewModel.kt
@@ -0,0 +1,11 @@
+package jp.mochili.mochili.viewmodel
+
+import android.databinding.ObservableField
+
+/**
+ * Created by ryotayamagishi on 2018/03/25.
+ */
+class AddFriendViewModel {
+    // databinding
+    val userId = ObservableField<String>()
+}

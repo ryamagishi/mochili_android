e5783fed182f22790af7dcfa5e5710916223bc38
Ryota Yamagishi <ddzsryotag@gmail.com>
2018-05-11 06:19:22 +0900

    mochilli画面を少し整えてコミット

--- app/src/main/kotlin/jp/mochili/mochili/view/MochiliActivity.kt
+++ app/src/main/kotlin/jp/mochili/mochili/view/MochiliActivity.kt
@@ -3,10 +3,12 @@ package jp.mochili.mochili.view
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import jp.mochili.mochili.R
 import jp.mochili.mochili.contract.MochiliViewContract
 import jp.mochili.mochili.databinding.ActivityMochiliBinding
 import jp.mochili.mochili.viewmodel.MochiliViewModel
+import kotlinx.android.synthetic.main.activity_create_mochili.*
 
 class MochiliActivity : AppCompatActivity(), MochiliViewContract {
 
@@ -18,5 +20,22 @@ class MochiliActivity : AppCompatActivity(), MochiliViewContract {
                 R.layout.activity_mochili)
         viewModel = MochiliViewModel(this)
         binding.viewmodel = viewModel
+
+        setView()
+    }
+
+    private fun setView() {
+        // actionbar
+        setSupportActionBar(my_toolbar)
+        supportActionBar?.title = "持ち物リスト"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    // 左上のbackと通常backの挙動を合わせる
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
